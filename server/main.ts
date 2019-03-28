e9f5ddda86a1dfbc76cf0431839b37cc28d31e92
Jesper Radegran <jradegra@tibco.com>
2019-03-28 21:55:27 +0100

    express

---
+++ server/main.ts
@@ -0,0 +1,16 @@
+const express = require( "express" );
+const path = require("path");
+const app = express();
+const port = 80; // default port to listen
+
+app.use( express.static( path.join( __dirname, "../dist" ) ) );
+
+// define a route handler for the default home page
+// app.get( "/", ( req, res ) => {
+//     res.send( "Hello world!" );
+// } );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
